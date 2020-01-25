c9ee09ddddab89c7f35d1f6d9f493d846c1f39cd
totlmstr <totlmstr@gmail.com>
2020-01-25 01:33:16 -0800

    Remove unneeded files

--- build.cmd
+++ build.cmd
@@ -1,20 +1,11 @@
 bcp ^
     filesystem ^
-    program_options ^
-    system ^
-	optional ^
-	^
-	log ^
-	config ^
-	predef ^
-    bootstrap.bat ^
-    bootstrap.sh ^
-    boostcpp.jam ^
-    boost-build.jam ^
-    tools/build ^
-    tools/boost_install/BoostConfig.cmake ^
-    tools/boost_install/BoostDetectToolset.cmake ^
-    tools/boost_install/boost-install.jam ^
-    tools/boost_install/BoostDetectToolset.cmake ^
-    tools/boost_install/boost-install-dirs.jam ^
+    optional ^
+    boost/mp11/integer_sequence.hpp ^
+    boost/mp11/bind.hpp ^
+    ^
+    build ^
+    libs/config/checks ^
+    libs/predef ^
+    libs/headers ^
     --boost="%1" .
